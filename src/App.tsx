1ed65be0f0bb31591b8adaf9f219795521602ec2
ravinduiddamalgoda <ravinduiddamalgoda55@gmail.com>
2024-10-23 02:07:35 +0530

    App.JSX changes

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,9 @@ const App: React.FC = () => {
 
   return (
     <div>
-        <div className="solana-connection">
-          <WalletConnectButton className='!bg-helius-orange hover:!bg-black transition-all duration-200 !rounded-lg' />
-        </div>
+      <div className="solana-connection">
+        <SolanaConnection />
+      </div>
     </div>
   );
 };
