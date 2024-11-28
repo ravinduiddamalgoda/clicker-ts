fedb06ce155334929e887f9a0611156f18d74219
ravinduiddamalgoda <ravinduiddamalgoda55@gmail.com>
2024-11-29 02:40:42 +0530

    :fix

--- config-overrides.js
+++ config-overrides.js
@@ -3,15 +3,16 @@ const webpack = require("webpack");
 module.exports = function override(config) {
   config.resolve.fallback = {
     ...config.resolve.fallback,
-    "crypto": require.resolve("crypto-browserify"),
-    "stream": require.resolve("stream-browserify"),
-    "http": require.resolve("stream-http"),
-    "https": require.resolve("https-browserify"),
-    "zlib": require.resolve("browserify-zlib"),
-    "events": require.resolve("events/"),
-    "util": require.resolve("util/"),
-    "assert": require.resolve("assert/"),
-    "string_decoder": require.resolve("string_decoder/"),
+    crypto: require.resolve("crypto-browserify"),
+    stream: require.resolve("stream-browserify"),
+    http: require.resolve("stream-http"),
+    https: require.resolve("https-browserify"),
+    zlib: require.resolve("browserify-zlib"),
+    events: require.resolve("events/"),
+    util: require.resolve("util/"),
+    assert: require.resolve("assert/"),
+    string_decoder: require.resolve("string_decoder/"),
+    vm: require.resolve("vm-browserify"), // Added the vm polyfill
   };
 
   config.plugins = (config.plugins || []).concat([
