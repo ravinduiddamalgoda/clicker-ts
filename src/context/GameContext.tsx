fc386c77ec16c18287417405889e71efe699fbdb
ZDevlopment <159969245+ZDevlopment@users.noreply.github.com>
2025-02-27 17:26:48 +1100

    Add files via upload

--- src/context/GameContext.tsx
+++ src/context/GameContext.tsx
@@ -1,5 +1,5 @@
 
-import { createContext } from "react";
+import React, { createContext } from "react";
 import { myConstants } from "../config/config";
 
 interface GameData {
@@ -8,25 +8,61 @@ interface GameData {
   fCount: number;
   level: number;
   lastUpdated: number;
+  completedTasks: Record<string, number>;
 }
 
+interface Task {
+  id: number;
+  target: number;
+  category: string;
+  startTime: number; // start time
+  rate: number; // F$rate at the time task is started
+  progress: number;
+}
+
+interface ActiveTaskArray {
+  task: Task;
+
+}
+
+
 interface ContextGameData {
+  userId: string,
   fCount: number;
   level: number;
   myConstants: typeof myConstants;
   F$rate: number;
-  levelupRate: number;
+ // levelupRate: number;
   isLoading: boolean;
   currentView: string;
+  message: string;
+  showMessage: boolean;
+  completedTasks: Record<string, number>;
+  activeTaskArray: ActiveTaskArray[]; 
+  referalLink: string;
+//  referralCode: string; // code receieved through the link
+ 
+  setUserId: React.Dispatch<React.SetStateAction<string>>;
   setCurrentView: React.Dispatch<React.SetStateAction<string>>;
+  setMessage: React.Dispatch<React.SetStateAction<string>>;
+  setShowMessage:React.Dispatch<React.SetStateAction<boolean>>;
   setfCount: React.Dispatch<React.SetStateAction<number>>;
   setLevel: React.Dispatch<React.SetStateAction<number>>;
   setF$rate: React.Dispatch<React.SetStateAction<number>>;
-  handleLogin: (email: string, password: string) => Promise<void>;
-  handleRegister: (email: string, password: string, username: string) => Promise<void>;
+  handleLogin: (email: string) => Promise<void>;
+ // handleRegister: (email: string, password: string, username: string) => Promise<void>;
   handleGoogleAuth: () =>  Promise<void>;
   handleLogout: () =>  Promise<void>;
+  setCompletedTasks: React.Dispatch<React.SetStateAction<Record<string, number>>>;
+  setActiveTaskArray: React.Dispatch<React.SetStateAction<ActiveTaskArray[]>>;
+  isDraw: boolean;
+  setIsDraw: React.Dispatch<React.SetStateAction<boolean>>;
+ 
   isLoggedIn: boolean;
+  setIsLoggedIn: React.Dispatch<React.SetStateAction<boolean>>;
+  setIsLoading: React.Dispatch<React.SetStateAction<boolean>>; 
+  setReferalLink: React.Dispatch<React.SetStateAction<string>>;
+  
   // setLevelupRate: React.Dispatch<React.SetStateAction<number>>; 
 }
 
