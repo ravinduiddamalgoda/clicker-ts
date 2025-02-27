28016cb048255b59badf17f22ac13a29a0648f11
ZDevlopment <159969245+ZDevlopment@users.noreply.github.com>
2025-02-27 17:22:38 +1100

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,7 +19,9 @@ module.exports = {
         primary: "#3B82F6", // Tailwind's blue-500 for buttons
         textPrimary: "#E2E8F0", // Light gray for primary text
         textSecondary: "#94A3B8", // Dim gray for secondary text
-        darkwine: "#450733"
+        darkwine: "#450733",
+        aqua: '#59b2a2',
+        seablue: '#48cae4',
       },
       boxShadow: {
         glow: '0 0px 60px #bf04c2, 0 0 100px #1700ad, 0 0 120px #00b570',
@@ -29,7 +31,7 @@ module.exports = {
         vividGreenGlow: '0 0 30px #129900',
       },
       fontFamily: {
-        roadrage: ['Road Rage', 'sans-serif'],
+        roadrage: ['Atma', 'sans-serif'],
         spicyrice: ['Spicy Rice', 'serif'],
       },
       textShadow: {
